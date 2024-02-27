fd50cc63cfd1d43f5be481ec1162c8404dbf2b66
NorePeeters <157713696+NorePeeters@users.noreply.github.com>
2024-02-27 11:32:15 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 let fghj = 0
 basic.forever(function () {
-    if ((45 as any) < (90 as any) || (90 as any) < (135 as any)) {
+    fghj = input.compassHeading()
+})
+basic.forever(function () {
+    if (fghj < 90 && fghj < 135) {
         basic.showLeds(`
             . . # . .
             . # . . .
@@ -8,44 +11,41 @@ basic.forever(function () {
             . # . . .
             . . # . .
             `)
-        if ((135 as any) < (180 as any) || (180 as any) < (225 as any)) {
-            basic.showLeds(`
-                . . # . .
-                . . # . .
-                # . # . #
-                . # # # .
-                . . # . .
-                `)
-        }
-        if ((225 as any) < (270 as any) || (270 as any) < (315 as any)) {
-            basic.showLeds(`
-                . . # . .
-                . . . # .
-                # # # # #
-                . . . # .
-                . . # . .
-                `)
-        }
-        if ((315 as any) < (360 as any)) {
-            basic.showLeds(`
-                . . # . .
-                . # # # .
-                # . # . #
-                . . # . .
-                . . # . .
-                `)
-        }
-        if ((0 as any) < (45 as any)) {
-            basic.showLeds(`
-                . . # . .
-                . # # # .
-                # . # . #
-                . . # . .
-                . . # . .
-                `)
-        }
     }
-})
-basic.forever(function () {
-    fghj = input.compassHeading()
+    if (fghj < 180 && fghj < 225) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if ((225 as any) < (270 as any) || (270 as any) < (315 as any)) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+    if ((315 as any) < (360 as any)) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+    if ((0 as any) < (45 as any)) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
 })
