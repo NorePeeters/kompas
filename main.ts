df73b4ae78830309d1ec88502865bc962cb9f86c
NorePeeters <157713696+NorePeeters@users.noreply.github.com>
2024-02-27 11:07:25 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
+let fghj = 0
 basic.forever(function () {
-    if (45 < ((90 as any) < (135 as any))) {
+    if ((45 as any) < (90 as any) || (90 as any) < (135 as any)) {
         basic.showLeds(`
             . . # . .
             . # . . .
@@ -7,41 +8,44 @@ basic.forever(function () {
             . # . . .
             . . # . .
             `)
+        if ((135 as any) < (180 as any) || (180 as any) < (225 as any)) {
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                # . # . #
+                . # # # .
+                . . # . .
+                `)
+        }
+        if ((225 as any) < (270 as any) || (270 as any) < (315 as any)) {
+            basic.showLeds(`
+                . . # . .
+                . . . # .
+                # # # # #
+                . . . # .
+                . . # . .
+                `)
+        }
+        if ((315 as any) < (360 as any)) {
+            basic.showLeds(`
+                . . # . .
+                . # # # .
+                # . # . #
+                . . # . .
+                . . # . .
+                `)
+        }
+        if ((0 as any) < (45 as any)) {
+            basic.showLeds(`
+                . . # . .
+                . # # # .
+                # . # . #
+                . . # . .
+                . . # . .
+                `)
+        }
     }
-    if (135 < ((180 as any) < (225 as any))) {
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            # . # . #
-            . # # # .
-            . . # . .
-            `)
-    }
-    if (225 < ((270 as any) < (315 as any))) {
-        basic.showLeds(`
-            . . # . .
-            . . . # .
-            # # # # #
-            . . . # .
-            . . # . .
-            `)
-    }
-    if ((315 as any) < (360 as any)) {
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-            `)
-    }
-    if ((0 as any) < (45 as any)) {
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-            `)
-    }
+})
+basic.forever(function () {
+    fghj = input.compassHeading()
 })
